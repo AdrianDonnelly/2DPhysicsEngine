f2f6038271203d4c3c2cc3bac07f81bbd820211e
AdrianDonnelly <X00194620@mytudublin.ie>
2025-07-06 16:22:25 +0100

    Decided on using Ebitengine for rendering

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
+// Package declaration
 package main
 
-func main() {
+// Import statements
+import (
+	"image/color"
+	"log"
 
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	pointColor := color.RGBA{0xff, 0xff, 0x00, 0xff} // Yellow
+	vector.DrawFilledCircle(screen, 50, 50, 2, pointColor, false)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+// Func main
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
